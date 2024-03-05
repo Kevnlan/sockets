06402bb290d11c2144163f0d5e955a43cd4ade30
Kevin Langat <kevin.langat@nathandigital.com>
2024-03-05 16:45:20 +0300

    several changes have been made, latest is emiting and listening to socket channels

--- lib/home.dart
+++ lib/home.dart
@@ -12,9 +12,12 @@ class NotificationExample extends StatefulWidget {
 class _NotificationExampleState extends State<NotificationExample> {
   late IO.Socket socket;
 
+  bool isMessageReceived = false;
+  Map message = {};
+
   @override
   void initState() {
-    // initSocket();
+    initSocket();
     super.initState();
   }
 
@@ -39,7 +42,7 @@ class _NotificationExampleState extends State<NotificationExample> {
               Center(
                 child: ElevatedButton(
                   onPressed: () {
-                    initSocket(); 
+                    initSocket();
                   },
                   child: Text('data'),
                 ),
@@ -53,6 +56,30 @@ class _NotificationExampleState extends State<NotificationExample> {
                   child: Text('disconnect'),
                 ),
               ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    createUser('kevnlan', '#Polerity99', 'dev@kevlangat.com');
+                  },
+                  child: Text('create user'),
+                ),
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    sendMessage('polomana', 'kevnlan',
+                        'this is a test send message I see if it works');
+                  },
+                  child: Text('send message'),
+                ),
+              ),
+              if (message == {} || message.isEmpty)
+                SizedBox()
+              else
+                ListTile(
+                  title: Text('Message : ${message['message']}'),
+                  subtitle:  Text('Sender : ${message['senderID']}'),
+                )
             ],
           ),
         ),
@@ -61,18 +88,21 @@ class _NotificationExampleState extends State<NotificationExample> {
   }
 
   initSocket() {
-    socket = IO.io('http://192.168.100.14:7001', <String, dynamic>{
+    print('object 212');
+    socket = IO.io('http://192.168.100.122:3000', <String, dynamic>{
       'autoConnect': false,
       'transports': ['websocket'],
       'extraHeaders': {
         'authorization':
-            'Api_key eyJhbGciOiJSUzUxMiIsInR5cCI6IkFwaSBUb2tlbiJ9.eyJwYXlsb2FkIjoiMjE5MGM4YTA5MmFmOTI4Y2RlNDEtMDdlMmFiM2JlZjQ2ODZlYjg5NTMtODcwNjcyZmE1N2E3Y2U2MTM3MzI3NWRlYzgzMDZmYmYtZTJlMzI1YWEwNmE3OWNiOGU4OTBhODliYTlhZDNhYWI3YTY3YThiODExMmE5MjlkMmQ4MCIsImlhdCI6MTcwNDE5NjgyNCwiZXhwIjoxNzA2Nzg4ODI0LCJpc3MiOiJOYXRoYW4gJiBOYXRoYW4gRGlnaXRhbCJ9.xqE5pYavzfobUpHDujiae1HjMJiJ68sLEtamtARBYpw-81TeSFycnGSPeS9_KeqAZjWPiD-bZzEUWRdXQ0L0ogJ_LsLX8RSHBDerPW0R3u0PER_O3hEaCa2g3hbriHjxPPhmB00B0LfJh8LB51vhFctl40qr5eBeuxNjf04Pn5mFq15qPPiTA76gJ2p-gqvOvMxqe5CxB4chz3T3TvFjXJnNO7K1y3lqHvnZTE0WHDH9N6dO9mJ4U1DKvoy9A3dl2eAtSO67_CpT3yZRnHVHXRwTFVrc1Q5tz9tVWHjnXcb66URA88CQV_vOU63DTa8jyLie0JZCvT0xoohBs7nCVHpdzXvGor7rmI2CNR9ZuHxDBRS6BZlm2ipdHe15r9gJ8e5H3Y_QYsflVi-Nsjic3_cvBDG7CiTcyAEjf7bXJdAGbxNaXwbSj_LUCOU9ITS32WSgnrNWDkrpKYhmu2kjwC8DYLHyRU6nN2mhP8YpYOVbosYddGLcOQmTovpePkdz_Bx4kBtCPZUyRhSHVsaEoztsQRp_jFV3Kqz6zCGf0Y7hJzhAyvo25qhqhInlWlIcS3bGcsmpxARdtmVsuK1DmB7cdzQXNgfHF7IvYYVLSvkAkGljsLAK18bmALw1n8_uoiZJo-YGT_GizvAAxk17IFsLY-I3ACd7ueYXnfNSA0g',
+            'Api_key eyJhbGciOiJSUzUxMiIsInR5cCI6IkFwaSBUb2tlbiJ9.eyJwYXlsb2FkIjoiN2RjNTdkMWYyMjg4Y2Y0YzU3ZmItNjY4OWI5ZDQxOTYyYzE2OTkxMjktYTg4MjcyYTU4MmJkNGJkNDQyMDc1NGRmYzJhN2I2OWQtMzc5YTNlNjM4ZTRjYjMwOTZlZDVjOTc4YjI3ZDJhOTkwYzdkMzU4YzNiODdjZTk0MzM5YiIsImlhdCI6MTcwNTQxNDE2NSwiZXhwIjoxNzA4MDA2MTY1LCJpc3MiOiJOYXRoYW4gJiBOYXRoYW4gRGlnaXRhbCJ9.MU8pnHb7TQ09HP9ljASCac2N91eOIaDbO1Af42MnbHrkWYtSyzRdPD2br8eFn9iDHUoBw2zmNQlC78P7IUjgofdHacWaJo7MvSwZfPbenRKhhPdWzj-7kPrC-ffRagvSTXSWXkYDQcOg5sz-AFxO8mCN0llpzabd6rdmY4nwyYni9wFW_XO8BfhLY3dOfE3lkrGc0dn0EX0-zkMElETKoMjyxZmAdQT3sSG4reJYotsi_BxDiDtq9R_WapLSRpwWOspMUxUSf6E7aPcnKRAny6TGZPPKLfiDHne0fdFCqkZbVUijrytyyKMVxAgYlzU5pMOn3bpB-TfWfhiJKo_hhuYBT1Y8qT5IBk-daQmzR1YXilJMHD2DombYeqU6AdvoSH3gTrqQxY6OkZM65BjZPNn6KjDc6jiC-QTmCuN3SLxXETQlecUuebznmu2FPyLHcTyAW4y1yMrQy3voyv5fHbh5FGHk3-xbXntvF_3pMUDJVaNs4VLnuhbEwWKif_tzViurDZX_B6MLa9gySvFPHrY_h14o8Wua_5A4p1dnpx1X5rpeBsS8ERARXdoLLV76YURLXfXLgSHh8Ne8xkxrIJI2hFQWVmUgxC_tlCwFp6Ryo0Au3k1oHtKDhVPRb7aoi7it4NYxaJ5U_qNK-Voya8Y-HBa0qc-K-ExlHUzAhuM',
         'identity': '1234'
       },
     });
+    print('object 212');
     socket.connect();
     socket.onConnect((_) {
       print('Connection established');
+      authenticateUser('kevnlan', '#Polerity99');
     });
 
     socket.onDisconnect((_) => print('Connection Disconnection'));
@@ -84,7 +114,15 @@ class _NotificationExampleState extends State<NotificationExample> {
     socket.on('personal', (data) {
       print('object b jdc d jcd');
       print(data.toString());
-      Notify(data.toString());
+    });
+
+    socket.on('chat message', (data) {
+      print('object b jdc d jcd');
+      print(data.toString());
+      setState(() {
+        isMessageReceived = true;
+        message = data;
+      });
     });
     // socket.onConnectError((err) {
     //   print('error $err');
@@ -111,8 +149,34 @@ class _NotificationExampleState extends State<NotificationExample> {
               'https://protocoderspoint.com/wp-content/uploads/2021/05/Monitize-flutter-app-with-google-admob-min-741x486.png',
           notificationLayout: NotificationLayout.BigPicture),
       schedule:
-          NotificationInterval(interval: 61, timeZone: timezom, repeats: true),
+          NotificationInterval(interval: 1, timeZone: timezom, repeats: false),
     );
+  }
 
+  void createUser(String username, String password, String email) {
+    // Send the user data to the server with the 'create user' event
+    socket.emit('create user', {
+      'username': username,
+      'password': password,
+      'email': email,
+    });
+  }
+
+  void sendMessage(String senderID, String receiverID, String message) {
+    // Send the message to the server with the 'chat message' event
+    socket.emit('chat message', {
+      'senderID': senderID,
+      'receiverID': receiverID,
+      'message': message,
+    });
+  }
+
+  void authenticateUser(String username, String password) {
+    // Send the user ID to the server for authentication
+    print('v bjsnjsnjsn');
+    socket.emit('authenticate', {
+      'username': username,
+      'password': password,
+    });
   }
 }
